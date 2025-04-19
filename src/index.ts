a8f470b6f8c0a4be4a6dfb33302f39460913216f
deni9309 <deni9309@gmail.com>
2025-04-19 20:13:02 +0300

    add db connection config

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.route';
+import { connectDB } from './lib/db';
 
 dotenv.config();
+const port = process.env.PORT;
 
 const app = express();
-const port = process.env.PORT || 5001;
 
 app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server is running on PORT: ${port}`);
+  connectDB();
 });
